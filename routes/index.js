0a44a0ab01f6200d5ef19724e1bc6bea1212f167
Jenny Knuth <inkboco@gmail.com>
2015-12-01 19:47:42 -0700

    can add and display users

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  //all of our code goes here
+  //insert into users (name) values ('jeff')
+
+  var pg = require('pg');
+  var conString = "postgres://localhost/movie_club";
+
+
+  //this initializes a connection pool
+  //it will keep idle connections open for a (configurable) 30 seconds
+  //and set a limit of 20 (also configurable)
+  pg.connect(conString, function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    //put actual sql statement here
+    client.query('select * from users', [], function(err, result) {
+      //call `done()` to release the client back to the pool
+      done();
+
+      if(err) {
+        return console.error('error running query', err);
+      }
+      res.render('index', { users: result.rows });
+    });
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  //all of our code goes here
+  //insert into users (name) values ('jeff')
+
+  var pg = require('pg');
+  var conString = "postgres://localhost/movie_club";
+
+
+  //this initializes a connection pool
+  //it will keep idle connections open for a (configurable) 30 seconds
+  //and set a limit of 20 (also configurable)
+  pg.connect(conString, function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    //put actual sql statement here
+    client.query('insert into users (name) values($1)', [req.body.name], function(err, result) {
+      //call `done()` to release the client back to the pool
+      done();
+
+      if(err) {
+        return console.error('error running query', err);
+      }
+      res.redirect('/');
+    });
+  });
 });
 
 module.exports = router;
